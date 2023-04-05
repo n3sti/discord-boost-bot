6c6e3bf9ce59ad1ee56b3d763049ae6b51016847
x1-2023 <127646397+x1-2023@users.noreply.github.com>
2023-04-06 00:00:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord, os, json, hashlib
 from boosting import *
-from colorfy import color
+from pyreqs3 import color
 from auto import *
 if os.name == 'nt':
     import ctypes
